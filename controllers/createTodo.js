910f5912c4d262770c27fe01fe031ce2deaaa04a
AvinashSirohi121 <avinashsirohi86@gmail.com>
2024-05-22 23:45:02 +0530

    Second Commit

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -25,3 +25,117 @@ exports.createTodo = async (req, res) => {
     });
   }
 };
+
+exports.getAllTodo = async (req, res) => {
+  try {
+    const todo = await Todo.find({});
+    if (todo) {
+      res.status(200).json({
+        success: true,
+        data: todo,
+        message: "All Todo Data fetched successfully",
+      });
+    }
+  } catch (err) {
+    console.log("Error while getting all todo", err);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+};
+
+exports.getTodoById = async (req, res) => {
+  try {
+    let id = req.params.id;
+
+    const todo = await Todo.findById({ _id: id });
+    if (todo) {
+      res.status(200).json({
+        success: true,
+        data: todo,
+        message: `Todo data for given id => ${id}`,
+      });
+    }
+  } catch (err) {
+    console.log("Error while getting Todo Data for id =>", err);
+    res.status(500).json({
+      success: false,
+      err: err.message,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+exports.deleteTodoById = async (req, res) => {
+  try {
+    let id = req.params.id;
+
+    let todo = await Todo.findByIdAndDelete({ _id: id });
+
+    res.status(200).json({
+      success: false,
+      message: `Todo for id => ${id} deleted successfully`,
+      data: todo,
+    });
+  } catch (err) {
+    console.log("Error while deleting the todo", err);
+    res.status(500).json({
+      success: false,
+      error: err.message,
+      message: "Error in deleting the Todo",
+    });
+  }
+};
+
+exports.deleteAllTodo = async (req, res) => {
+  try {
+    let x = await Todo.deleteMany({});
+    if (x) {
+      res.status(200).json({
+        success: true,
+        data: `${x} no. of todos deleted`,
+        message: "Todo deleted successfully",
+      });
+    }
+  } catch (err) {
+    console.log("Error while deleting All todos");
+    res.status(500).json({
+      success: false,
+      err: err.message,
+      message: "Error while deleting All todos",
+    });
+  }
+};
+
+exports.updateTodo = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let { title, description } = req.body;
+
+    let todo = await Todo.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          title: title,
+          description: description,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: todo,
+      message: `Todo => ${id} updated successfully`,
+    });
+  } catch (err) {
+    console.log("Error while updating the todo");
+    res.status(500).json({
+      success: false,
+      err: err.message,
+      message: "Error while updating the todo",
+    });
+  }
+};
--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,19 @@
-const {createTodo} = require("../controllers/createTodo");
+const {
+  createTodo,
+  getAllTodo,
+  getTodoById,
+  deleteTodoById,
+  deleteAllTodo,
+  updateTodo
+} = require("../controllers/createTodo");
 const express = require("express");
 const router = express.Router();
 
 // API routes for creating Todo
 router.post("/createTodo", createTodo);
-
+router.get("/getTodos", getAllTodo);
+router.get("/getTodoById/:id",getTodoById)
+router.delete("/deleteTodoById/:id",deleteTodoById)
+router.delete("/deleteAllTodo",deleteAllTodo);
+router.put("/updateTodo/:id",updateTodo);
 module.exports = router;
